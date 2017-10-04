4a404f91e8877f114d6ec3de0461039b29ed11c3
revaxl <robin208@hotmail.fr>
2017-10-04 19:16:58 +0100

    added hard-coded interfaces and routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/reset-password', function(req, res){
+  res.render('reset');
+});
+
+router.get('/dashboard', function(req, res){
+  res.render('dashboard');
+});
+
+router.get('/metro', function(req, res){
+  res.render('metros');
+});
+
+router.get('/affections', function(req, res){
+  res.render('affections');
+});
+
+router.get('/tracking', function(req, res){
+  res.render('tracking');
+});
+
+router.get('/problems', function(req, res){
+  res.render('problems');
 });
 
 module.exports = router;
